a08aa5a57d21bdf2a24b67dbf8b407e04a8524e7
𝑶𝒔𝒂𝒕𝒐𝒏𝒊𝒄𝘍𝘳𝘰𝘮𝑩𝒓𝒂𝒛𝒛𝒆𝒓𝒔 <osatonic@gmail.com>
2026-09-25 03:00:59 +0500

    Add lab7 file streams laboratory work

---
+++ lab7/main3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+
+#include "TransportFile.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const char* fileName = "transport.dat";
+
+    ofstream out(fileName, ios::binary | ios::app);
+
+    if (!out)
+    {
+        cerr << "Ошибка открытия файла!" << endl;
+        return 1;
+    }
+
+    int count;
+
+    cout << "Сколько объектов добавить? ";
+    cin >> count;
+
+    for (int i = 0; i < count; i++)
+    {
+        TransportFile object;
+
+        cout << "\nНовый объект №" << i + 1 << endl;
+
+        cin >> object;
+
+        out.write(
+            reinterpret_cast<const char*>(&object),
+            sizeof(TransportFile)
+        );
+    }
+
+    out.close();
+
+    ifstream in(fileName, ios::binary);
+
+    if (!in)
+    {
+        cerr << "Ошибка открытия файла!" << endl;
+        return 1;
+    }
+
+    in.seekg(0, ios::end);
+
+    long fileSize = in.tellg();
+    int objectCount = fileSize / sizeof(TransportFile);
+
+    in.close();
+
+    cout << "\nПосле добавления:" << endl;
+    cout << "Количество объектов: " << objectCount << endl;
+    cout << "Размер файла: " << fileSize << " байт" << endl;
+
+    return 0;
+}
