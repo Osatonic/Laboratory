21d8fd55f5b3e65e5c0a0008942288c1d43bbb07
𝑶𝒔𝒂𝒕𝒐𝒏𝒊𝒄𝘍𝘳𝘰𝘮𝑩𝒓𝒂𝒛𝒛𝒆𝒓𝒔 <osatonic@gmail.com>
2026-09-25 03:01:01 +0500

    Add lab7 file streams laboratory work

---
+++ lab7/main4.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdio>
+
+#include "TransportFile.h"
+
+using namespace std;
+
+long find(ifstream& file, const char* key)
+{
+    TransportFile object;
+
+    file.clear();
+    file.seekg(0, ios::beg);
+
+    long position = 0;
+
+    while (file.read(
+        reinterpret_cast<char*>(&object),
+        sizeof(TransportFile)))
+    {
+        if (strcmp(object.GetName(), key) == 0)
+            return position;
+
+        position += sizeof(TransportFile);
+    }
+
+    return -1;
+}
+
+void del(const char* fileName, long position)
+{
+    ifstream in(fileName, ios::binary);
+
+    if (!in)
+    {
+        cout << "Ошибка открытия исходного файла!" << endl;
+        return;
+    }
+
+    ofstream out("temp.dat", ios::binary);
+
+    if (!out)
+    {
+        cout << "Ошибка создания временного файла!" << endl;
+        in.close();
+        return;
+    }
+
+    TransportFile object;
+    long currentPosition = 0;
+
+    while (in.read(
+        reinterpret_cast<char*>(&object),
+        sizeof(TransportFile)))
+    {
+        if (currentPosition != position)
+        {
+            out.write(
+                reinterpret_cast<const char*>(&object),
+                sizeof(TransportFile)
+            );
+        }
+
+        currentPosition += sizeof(TransportFile);
+    }
+
+    in.close();
+    out.close();
+
+    remove(fileName);
+    rename("temp.dat", fileName);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const char* fileName = "transport.dat";
+    char key[50];
+
+    cout << "Введите название транспорта для удаления: ";
+    cin >> key;
+
+    ifstream file(fileName, ios::binary);
+
+    if (!file)
+    {
+        cerr << "Не удалось открыть файл!" << endl;
+        return 1;
+    }
+
+    long position = find(file, key);
+    file.close();
+
+    if (position == -1)
+    {
+        cout << "Объект не найден." << endl;
+        return 0;
+    }
+
+    cout << "Объект найден." << endl;
+    cout << "Смещение от начала файла: "
+         << position << " байт" << endl;
+
+    del(fileName, position);
+
+    cout << "Объект удалён." << endl;
+
+    return 0;
+}
