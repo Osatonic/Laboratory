e13dffade2f1558e8ec665cab116f8fa3c679238
𝑶𝒔𝒂𝒕𝒐𝒏𝒊𝒄𝘍𝘳𝘰𝘮𝑩𝒓𝒂𝒛𝒛𝒆𝒓𝒔 <osatonic@gmail.com>
2026-09-25 03:00:49 +0500

    Add lab7 file streams laboratory work

---
+++ lab7/TransportFile.h
@@ -0,0 +1,35 @@
+#ifndef TRANSPORTFILE_H
+#define TRANSPORTFILE_H
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class TransportFile
+{
+private:
+    char name[50];
+    int speed;
+    int doors;
+
+public:
+    TransportFile();
+    TransportFile(const char* n, int s, int d);
+    TransportFile(const TransportFile& other);
+    ~TransportFile();
+
+    TransportFile& operator=(const TransportFile& other);
+
+    void Input();
+    void Show() const;
+
+    const char* GetName() const;
+
+    friend ostream& operator<<(ostream& out, const TransportFile& object);
+    friend istream& operator>>(istream& in, TransportFile& object);
+
+    bool operator==(const TransportFile& other) const;
+};
+
+#endif
